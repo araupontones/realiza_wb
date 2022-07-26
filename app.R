19c3f7e870aa1b93181000d01c459d88e855a9a2
araupontones <43657130+araupontones@users.noreply.github.com>
2022-07-26 12:26:43 +0200

    fuction to uptadate zoho

--- app.R
+++ app.R
@@ -49,6 +49,17 @@ ui <- fluidPage(
                         )
              ),
              
+             tabPanel("Feedback",
+                      tags$iframe(src= "https://app.powerbi.com/view?r=eyJrIjoiZWE2ZjA0NWItYWE0NS00M2M4LThjNWUtMzFmOTMzMWM0NDMwIiwidCI6IjFmMTU1ZTFlLWQyZGYtNDYzYi04NDZjLWM4NzJiZWU0Yjg0NCJ9&pageName=ReportSection19c755afbc835e848d83",
+                                  width = "100%",
+                                  height= "600px",
+                                  frameborder="0",
+                                  allowfullscreen="true"
+                                  
+                                  )
+                      
+                      ),
+             
              
              tabPanel("Admin",
                       ui_admin("admin"))
