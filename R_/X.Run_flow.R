687264397f17efafe548f45b5de9ed85ff18acc4
araupontones <43657130+araupontones@users.noreply.github.com>
2022-08-03 14:38:22 +0200

    merge

--- R_/X.Run_flow.R
+++ R_/X.Run_flow.R
@@ -4,29 +4,30 @@ for_dashboard <- list.files("R_/2.create_data_dashboard", full.names = T)
 dir_lkps <- "data/0look_ups"
 
 
-# 
-# #Update look ups
-# lps <- lapply(lp_scripts, function(x){
-#   
-#   source(x, encoding = "UTF-8")
-#   
-# })
-# 
-# 
-# #Downlaod data from zoho
-# #Append groups and individual sessions
-# #Clean data
-# zoho_data <- lapply(zoho_scripts, function(x){
-#   
-#   
-#   source(x, encoding = "UTF-8")
-#   
-# })
+
+#Update look ups
+lps <- lapply(lp_scripts, function(x){
+
+  source(x, encoding = "UTF-8")
+
+})
+
+
+#Downlaod data from zoho
+#Append groups and individual sessions
+#Clean data
+zoho_data <- lapply(zoho_scripts, function(x){
+
+
+  source(x, encoding = "UTF-8")
+
+})
 
 
 dashboard_data <- lapply(for_dashboard, function(x){
   
   source(x, encoding = "UTF-8")
+  
 })
 
 
