3f67c8a115cdd7f66de4b7ddf849fa8406f28094
araupontones <43657130+araupontones@users.noreply.github.com>
2022-11-10 13:55:40 +0100

    fix count of participants

--- R/module_totais.R
+++ R/module_totais.R
@@ -63,42 +63,38 @@ serverTotals<- function(id, dir_data) {
       filter(Status == "Presente")
     
 
-            
-    
-    
-    ##identify emprendedoras that attended to the first session
-    inaugural<-  presencas %>% 
-      filter(actividade =="Sessão Inaugural") %>%
-      select(ID_BM, Status)
-    
-    
-    ##identify emprendedoras that attended to the primera sessao
-    primera<- presencas %>%
-      group_by(ID_BM) %>%
-      #filter(data_posix == min(data_posix)) %>%
-      slice(1) %>%
-      ungroup() %>%
-      mutate(actividade = "Primera sessao") %>%
-      select(ID_BM, Status_primera = Status)
-    
-    
-    
-    ## Join lookup of emprendedoras with peresencas of first session
-    data_totais <- emprendedoras %>%
+    # all emprendedoras nas listas do banco mundial
+    listas_BM <- emprendedoras %>%
+      mutate(interesada = status_realiza == "CONFIRMADA",
+             nas_listas = TRUE,
+             across(c(interesada, nas_listas), function(x)ifelse(x, 1,0))) %>%
       select(ID_BM, Cidade, 
              Abordagem = grupo_accronym,
-             status_realiza) %>%
-      left_join(inaugural, by = "ID_BM") %>%
-      left_join(primera, by = "ID_BM") %>%
-      group_by(Abordagem, Cidade) %>%
-      ##Count total in WB data, Confirmadas, and those who attended the first session
-      summarise("Nas Listas BM" = n(),
-                `Interesadas em participar` = sum(status_realiza == "CONFIRMADA", na.rm = T),
-                `Tem participado` = sum(!is.na(Status_primera)),
-                `Veio sessao inaugural` = sum(!is.na(Status)),
-                
-                .groups = 'drop'
+             interesada,
+             nas_listas
+      )
+    
+    
+    #participations
+    participants <- presencas %>%
+      mutate(inaugural = actividade =="Sessão Inaugural") %>%
+      group_by(ID_BM) %>%
+      summarise(inaugural = max(inaugural),
+                participado = 1,
+                .groups = 'drop') 
+    
+    
+    #join both
+    data_totais <- listas_BM %>%
+      left_join(participants, by = "ID_BM") %>%
+      #rename for presentation
+      rename(`Nas Listas BM` = nas_listas,
+             `Interesadas em participar` = interesada,
+             `Tem participado` = participado,
+             `Veio sessao inaugural` = inaugural
       ) %>%
+      select(-ID_BM) %>%
+      #reshape for visuals
       pivot_longer(-c(Abordagem, Cidade),
                    names_to = "Status") %>%
       mutate(Status = factor(Status,
@@ -106,8 +102,53 @@ serverTotals<- function(id, dir_data) {
                                         "Interesadas em participar",
                                         "Tem participado",
                                         "Veio sessao inaugural"
-                                       
+                                        
                              )))
+    names(data_totais)      
+    
+    # 
+    # ##identify emprendedoras that attended to the first session
+    # inaugural<-  presencas %>% 
+    #   filter(actividade =="Sessão Inaugural") %>%
+    #   select(ID_BM, Status)
+    # 
+    # 
+    # ##identify emprendedoras that attended to the primera sessao
+    # primera<- presencas %>%
+    #   group_by(ID_BM) %>%
+    #   #filter(data_posix == min(data_posix)) %>%
+    #   slice(1) %>%
+    #   ungroup() %>%
+    #   mutate(actividade = "Primera sessao") %>%
+    #   select(ID_BM, Status_primera = Status)
+    # 
+    # 
+    # 
+    # ## Join lookup of emprendedoras with peresencas of first session
+    # data_totais <- emprendedoras %>%
+    #   select(ID_BM, Cidade, 
+    #          Abordagem = grupo_accronym,
+    #          status_realiza) %>%
+    #   left_join(inaugural, by = "ID_BM") %>%
+    #   left_join(primera, by = "ID_BM") %>%
+    #   group_by(Abordagem, Cidade) %>%
+    #   ##Count total in WB data, Confirmadas, and those who attended the first session
+    #   summarise("Nas Listas BM" = n(),
+    #             `Interesadas em participar` = sum(status_realiza == "CONFIRMADA", na.rm = T),
+    #             `Tem participado` = sum(!is.na(Status_primera)),
+    #             `Veio sessao inaugural` = sum(!is.na(Status)),
+    #             
+    #             .groups = 'drop'
+    #   ) %>%
+    #   pivot_longer(-c(Abordagem, Cidade),
+    #                names_to = "Status") %>%
+    #   mutate(Status = factor(Status,
+    #                          levels = c("Nas Listas BM",
+    #                                     "Interesadas em participar",
+    #                                     "Tem participado",
+    #                                     "Veio sessao inaugural"
+    #                                    
+    #                          )))
     
     
     
@@ -160,7 +201,7 @@ serverTotals<- function(id, dir_data) {
       
       data_plot <- data_totais %>%
         group_by_at(agrupar_por) %>%
-        summarise(value = sum(value),
+        summarise(value = sum(value, na.rm = T),
                   .groups = 'drop') %>%
        dplyr::filter(Status %in% status_vector[as.numeric(input$check_group)])
       
